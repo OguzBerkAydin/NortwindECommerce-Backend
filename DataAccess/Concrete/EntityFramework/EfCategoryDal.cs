e2883df69631937e5f10893f899c1bd688340549
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2024-08-31 21:26:21 +0300

    #

--- DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
@@ -1,45 +1,13 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-	public class EfCategoryDal : ICategoryDal
+	public class EfCategoryDal : EfEntityRepositoryBase<Category, NorthwindContext>, ICategoryDal
 	{
-		public void Add(Category entity)
-		{
-			using (NorthwindContext context = new())
-			{
-				var addedEntity = context.Entry(entity);
-				addedEntity.State = EntityState.Added;
-				context.SaveChanges();
-			}
-		}
-
-		public void Delete(Category entity)
-		{
-			using (NorthwindContext context = new())
-			{
-				var deletedEntity = context.Entry(entity);
-				deletedEntity.State = EntityState.Deleted;
-				context.SaveChanges();
-			}
-		}
-
-		public Category Get(Expression<Func<Category, bool>> filter)
-		{
-			throw new NotImplementedException();
-		}
-
-		public List<Category> GetAll(Expression<Func<Category, bool>> filter = null)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void Update(Category entity)
-		{
-			throw new NotImplementedException();
-		}
+		
 	}
 }
