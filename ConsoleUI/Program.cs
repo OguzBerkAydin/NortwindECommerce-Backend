9946f237c9bdf8b1465ade39ec5352d2883b8878
Oguz Berk AYDIN <104473831+SosisliYumurta@users.noreply.github.com>
2024-09-27 00:00:34 +0300

    Autofac business module added. IoC container refoctered for Autofac

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -19,23 +19,23 @@ namespace ConsoleUI
 
         }
 
-		private static void CategoryTest()
-		{
-			CategoryManager categoryManager = new(new EfCategoryDal());
-			foreach (var category in categoryManager.GetAll())
-			{
-				Console.WriteLine(category.CategoryName);
-			}
-		}
+		//private static void CategoryTest()
+		//{
+		//	CategoryManager categoryManager = new(new EfCategoryDal());
+		//	foreach (var category in categoryManager.GetAll())
+		//	{
+		//		Console.WriteLine(category.CategoryName);
+		//	}
+		//}
 
-		private static void ProductTest()
-		{
-			ProductManager productManager = new(new EfProductDal());
+		//private static void ProductTest()
+		//{
+		//	ProductManager productManager = new(new EfProductDal());
 
-			foreach (var product in productManager.GetAll())
-			{
-				Console.WriteLine(product.ProductName);
-			}
-		}
+		//	foreach (var product in productManager.GetAll())
+		//	{
+		//		Console.WriteLine(product.ProductName);
+		//	}
+		//}
 	}
 }
